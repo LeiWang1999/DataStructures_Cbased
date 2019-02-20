6ceb27ec59ab08a5da30fa93e0e8761985e46ca1
NjtechPrinceling <2230647190@qq.com>
2019-02-20 22:48:28 +0800

    添加了surface分支喽。

--- TEST/xujiayi.c
+++ TEST/xujiayi.c
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[])
 {
     double cost;
     char a[2];
+    printf("请开始输入吧\n");
 	while(scanf("**** %lf %s折", &cost, a)){
         printf("价格为:%d,折扣为%s\n",cost,a);
         getchar();
